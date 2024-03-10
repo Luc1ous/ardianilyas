f3f7a3e1028ff704e3285835aa78882208269fea
Luc1ous <ardianilyas@gmail.com>
2024-03-10 21:36:42 +0700

    push all project

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: "https://fonts.googleapis.com" },
+        { rel: 'preconnect', href: "https://fonts.gstatic.com" },
+        { rel: 'stylesheet' ,href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" }
+      ]
+    }
+  }
+})
